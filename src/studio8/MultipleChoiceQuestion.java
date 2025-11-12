e86601232d8b0f137979261d4ec4c08a44875a8b
Keon Preusser <keonp@wustl.edu>
2025-11-12 13:58:55 -0600

    studio 8

--- src/studio8/MultipleChoiceQuestion.java
+++ src/studio8/MultipleChoiceQuestion.java
@@ -4,8 +4,11 @@ import support.cse131.NotYetImplementedException;
 
 public class MultipleChoiceQuestion extends Question {
 
+	private String[] choices;
+
 	/**
 	 * Constructor
+	 * 
 	 * @param prompt
 	 * @param answer
 	 * @param points
@@ -15,27 +18,39 @@ public class MultipleChoiceQuestion extends Question {
 		// Call the super class constructor, then create and set
 		// instance variables for any values that aren't handled
 		// by the base class
-		throw new NotYetImplementedException();
+		super(prompt, answer, points);
+		this.choices = choices;
 	}
-	
+
 	/**
-	 * Display the prompt for the question in addition to 
+	 * Display the prompt for the question in addition to
 	 * the choices present for the question.
 	 */
 	public void displayPrompt() {
-		throw new NotYetImplementedException();
+		super.displayPrompt();
+		for (int i = 0; i < choices.length; i++) {
+			System.out.println((i + 1) + ". " + choices[i]);
+		}
 	}
-	
+
 	/**
 	 * Getter method for the available choices
+	 * 
 	 * @return String[] of choices
 	 */
 	public String[] getChoices() {
-		throw new NotYetImplementedException();
+		return choices;
 	}
-	
+
 	public static void main(String[] args) {
 		// TODO: create your own MultipleChoiceQuestion
+		String[] choices = { "1", "2", "3", "4", "5" };
+		Question q1 = new MultipleChoiceQuestion("What's my favorite number?", "5", 100, choices);
+
+		System.out.println(q1.getAnswer());
+		System.out.println(q1.getPoints());
+		q1.displayPrompt();
+		System.out.println(q1.checkAnswer("2"));
 	}
 
 }
